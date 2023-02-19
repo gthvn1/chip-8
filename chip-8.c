3d3fed7e62d0254e6d7d9b7ff38044fd74024401
Guillaume <gthvn1@gmail.com>
2023-02-19 21:09:20 +0100

    Add creation of renderer

--- chip-8.c
+++ chip-8.c
@@ -1,25 +1,32 @@
-#include <SDL2/SDL_error.h>
-#include <SDL2/SDL_events.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
 
-typedef struct {
-	uint8_t scale;
-	SDL_Window *window;
-} sdl_t;
+typedef enum {
+	CREATE_WINDOW_ERROR = 1,
+	CREATE_RENDERER_ERROR,
+} chip8_error;
 
 const uint32_t CHIP8_WINDOW_WIDTH = 64;
 const uint32_t CHIP8_WINDOW_LENGHT = 32;
 
+typedef struct {
+	uint8_t scale;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+} sdl_t;
+
 /* Set default values */
-void init_sdl(sdl_t *sdl)
+void new_sdl(sdl_t *sdl)
 {
 	sdl->scale = 10;
 }
 
-bool init_window(sdl_t *sdl, const char *title)
+bool create_window(sdl_t *sdl, const char *title)
 {
 	sdl->window = SDL_CreateWindow(
 			title,
@@ -36,17 +43,41 @@ bool init_window(sdl_t *sdl, const char *title)
 	return true;
 }
 
+bool create_renderer(sdl_t *sdl)
+{
+	assert(sdl->window != NULL);
+	sdl->renderer = SDL_CreateRenderer(
+			sdl->window,
+			-1,
+			SDL_RENDERER_ACCELERATED);
+	if (sdl->renderer == NULL) {
+		printf("Failed to create renderer: %s\n", SDL_GetError());
+		return false;
+	}
+
+	return true;
+}
+
 int main(int argc, char **argv)
 {
 	const char *title = "Chip-8 emulator";
 	sdl_t sdl = {0};
 
-	init_sdl(&sdl);
+	new_sdl(&sdl);
 
 	puts("Hello, World!");
-	if (!init_window(&sdl, title)) {
-		return 1;
+	if (!create_window(&sdl, title)) {
+		return CREATE_WINDOW_ERROR;
 	}
 
+	if (!create_renderer(&sdl)) {
+		SDL_DestroyWindow(sdl.window);
+		return CREATE_RENDERER_ERROR;
+	}
+
+	// Next step: SDL_SetRenderDrawColor !!!
+cleanup:
+	SDL_DestroyWindow(sdl.window);
+	SDL_DestroyRenderer(sdl.renderer);
 	return 0;
 }
