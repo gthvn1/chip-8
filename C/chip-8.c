cca9b74d3f1687cb43295c8995c994e4268dcc1f
Guillaume <gthvn1@gmail.com>
2023-02-20 10:38:41 +0100

    Start init chip8

--- C/chip-8.c
+++ C/chip-8.c
@@ -7,13 +7,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+const uint32_t CHIP8_WINDOW_WIDTH = 64;
+const uint32_t CHIP8_WINDOW_LENGHT = 32;
+
 typedef enum {
 	CREATE_WINDOW_ERROR = 1,
 	CREATE_RENDERER_ERROR,
+	ROM_FILENAME_IS_MISSING,
+	INIT_CHIP8_ERROR,
 } chip8_error;
-
-const uint32_t CHIP8_WINDOW_WIDTH = 64;
-const uint32_t CHIP8_WINDOW_LENGHT = 32;
 
 typedef struct {
 	uint8_t  ram[4096]; // 4 KB RAM
@@ -36,7 +38,6 @@ void init_sdl(sdl_t *sdl)
 {
 	sdl->scale = 10;
 	SDL_Init(SDL_INIT_VIDEO); // init SDL2
-
 }
 
 void cleanup_sdl(sdl_t *sdl)
@@ -66,6 +67,7 @@ bool create_window(sdl_t *sdl, const char *title)
 bool create_renderer(sdl_t *sdl)
 {
 	assert(sdl->window != NULL);
+
 	sdl->renderer = SDL_CreateRenderer(
 			sdl->window,
 			-1,
@@ -78,9 +80,26 @@ bool create_renderer(sdl_t *sdl)
 	return true;
 }
 
+bool init_chip8(chip8_t *chip8, char *filename)
+{
+	// TODO:
+	// - Open the file for reading
+	// - Load the content of the file in RAM at 0x200
+	// - Set the PC to 0x200
+	// - Set the SP ???
+	// - Set the 2 delay timers ???
+	return true;
+}
+
+void help(char *progname)
+{
+	printf("USAGE: %s <rom>\n", progname);
+}
+
 int main(int argc, char **argv)
 {
 	const char *title = "Chip-8 emulator";
+	chip8_t chip8 = {0};
 	sdl_t sdl = {0};
 
 	init_sdl(&sdl); // Set default value and init SDL2
@@ -98,7 +117,19 @@ int main(int argc, char **argv)
 	SDL_RenderClear(sdl.renderer); // clear screen with our color.
 	SDL_RenderPresent(sdl.renderer); // Show the modifications
 
+	// check if there is at least one argument and init chip8
+	if (argc < 2) {
+		cleanup_sdl(&sdl);
+		help(argv[0]);
+		return ROM_FILENAME_IS_MISSING;
+	}
+	if (!init_chip8(&chip8, argv[1])) {
+		cleanup_sdl(&sdl);
+		return INIT_CHIP8_ERROR;
+	}
+
 	// Before closing wait 3 Seconds...
+	// The event loop will soon replace this delay
 	SDL_Delay(3000);
 
 	cleanup_sdl(&sdl);
